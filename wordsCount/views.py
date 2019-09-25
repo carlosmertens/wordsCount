37bbbf51d948ce991255b4214b7bad3c5419edf1
Carlos Mertens <mertenscarlos@yahoo.com>
2019-09-25 21:12:50 +0200

    Create words form linked to word page

--- wordsCount/views.py
+++ wordsCount/views.py
@@ -5,3 +5,9 @@ def home(request):
     """Funtion to return home page."""
 
     return render(request, 'home.html')
+
+
+def count(request):
+    """Function to return count page."""
+
+    return render(request, 'count.html')
